d7bc3650f96bb6ab119822deee537fd812501e67
Williams <o1-williams@hoare7.cs.umsl.edu>
2016-10-08 19:54:56 -0500

    Concurrency implemented

--- master.c
+++ master.c
@@ -12,13 +12,13 @@
 
 /* create num_slaves slave processes that will write to a file max_writes 
  * times*/
-void spawnSlaves(int num_slaves, int max_writes)
+void spawnSlaves(int num_slaves, int max_writes, char *filename)
 {
 	int i;
 	pid_t pids[num_slaves];
-	char arg1[20];	// holds the process number
+	char arg1[20];	// holds the rank of the child (between 0 and num_slaves -1)
 	char arg2[20];	// holds the number of times to write to file
-
+	char arg3[20];	// holds the number of slaves that are being generated
 	for (i = 0; i < num_slaves; i++)
 	{
 		if ((pids[i] = fork()) < 0)
@@ -28,29 +28,25 @@ void spawnSlaves(int num_slaves, int max_writes)
 		}
 		else if (pids[i] == 0)
 		{
-			snprintf(arg1, sizeof(arg1), "%d", getpid());
+			snprintf(arg1, sizeof(arg1), "%d", i);
 			snprintf(arg2, sizeof(arg2), "%d", max_writes);
-			execl("./slave", "slave", arg1, arg2, NULL);
+			snprintf(arg3, sizeof(arg3), "%d", num_slaves);
+			execl("./slave", "slave", arg1, arg2, arg3, filename, NULL);
 			exit(0);
 		}
 	}	
 }
 
-CreateSharedMemory()
-{
-	
-}
-
 int main(int argc, char *argv[])
 {
-	int        opt;                                                        /* holds the current command line argument */
-	const char *short_opt        = "hs:l:i:t:";                            /* valid short cmd args */
-	struct     option long_opt[] = { { "help", no_argument, NULL, 'h' } }; /* valid long cmd args */
-	int        num_slaves        = 5;                                      /* max number of slave processes */
-	char       *filename         = "test.out";                             /* log file for output */
-	int        num_increment     = 3;                                      /* how many times a slave should increment the value before terminating */
-	int        kill_time         = 20;                                     /* amount of seconds before master will terminate itself */
-	
+	int			opt;                                                        /* holds the current command line argument */
+	const char	*short_opt        = "hs:l:i:t:";                            /* valid short cmd args */
+	struct		option long_opt[] = { { "help", no_argument, NULL, 'h' } }; /* valid long cmd args */
+	int			num_slaves        = 5;                                      /* max number of slave processes */
+	char		*filename         = "test.out";                             /* log file for output */
+	int			num_increment     = 3;                                      /* how many times a slave should increment the value before terminating */
+	int			kill_time         = 20;                                     /* amount of seconds before master will terminate itself */
+	int			i;															/* iterator */
 
 	/* process cmd args */
 	while ((opt = getopt_long(argc, argv, short_opt, long_opt, NULL)) != -1)
@@ -69,6 +65,7 @@ int main(int argc, char *argv[])
 				printf("    -t [z]        the time in seconds when the master will terminate itself (default: 20)\n");
 				exit(0);
 				break;
+			// insert error checking...
 			case 's':
 				num_slaves = atoi(optarg);
 				break;
@@ -84,51 +81,57 @@ int main(int argc, char *argv[])
 			default:
 				printf("%s: invalid option -- %c\n", argv[0], opt);
 				printf("Try '%s --help' for more information.\n", argv[0]);
-				return -2;
+				exit(1);
 		}
 	};
+
+	// clear out the log file of any old content so we can properly append later
+	FILE *fp;
+	fp = fopen(filename, "w");
+	fclose(fp);
 	
-	int shmid;
-	key_t key;
-	int shm_val = 0;
-	int *shm = &shm_val;
+	// all of our shared memory variables
+	int turn;
+	int flag[num_slaves]; // 0=idle, 1=want_in, 2=in_cs
+	int sharedNum;
 
-	// name the shared memory segment
-	//if ((key = ftok("/tmp", 199)) == -1)
-	//{
-	//	perror("ftok");
-	//	exit(1);
-	//}
-	key = 5678;
+	key_t key_turn = ftok("/tmp", 35);
+	int shmid_turn = shmget(key_turn, sizeof(int), IPC_CREAT | 0666);
+	int *shm_turn = shmat(shmid_turn, NULL, 0);
+	turn = *shm_turn;
+	
+	key_t key_flag = ftok("/tmp", 93);
+	int shmid_flag = shmget(key_flag, sizeof(int)*num_slaves, IPC_CREAT | 0666);
+	int *shm_flag = shmat(shmid_flag, NULL, 0);
+	for (i = 0; i < num_slaves; i++) flag[i] = *(shm_flag + i);
 
-	// create the segment
-	if ((shmid = shmget(key, 27, IPC_CREAT | 0666)) < 0)
-	{
-		perror("shmget");
-		exit(1);
-	}
+	key_t key_shnum = ftok("/tmp", 27);
+	int shmid_shnum = shmget(key_shnum, sizeof(int), IPC_CREAT | 0666);
+	int *shm_shnum = shmat(shmid_shnum, NULL, 0);
+	sharedNum = *shm_shnum;
 
-	// attach the segment to our data space
-	if ((shm = shmat(shmid, NULL, 0)) == (int *) -1)
-	{
-		perror("shmat");
-		exit(1);
-	}
-
-	spawnSlaves(num_slaves, num_increment);
+	spawnSlaves(num_slaves, num_increment, filename);
 
 	int status;
 	pid_t pid;
 	while (num_slaves > 0)
 	{
 		pid = wait(&status);
-		//printf("Child with PID %ld has died :(\n", (long)pid, status);
+		fprintf(stderr, "Child with PID %ld has died :(\n", (long)pid, status);
 		--num_slaves;
 	}
 	
 	// cleanup
-	(*shm) = 0;
-	shmdt(shm);
-	shmctl(shmid, IPC_RMID, NULL);
+	fprintf(stderr, "Master - I am clearing memory now\n");
+	
+	shmdt(shm_shnum);
+	shmdt(shm_turn);
+	shmdt(shm_flag);
+	
+	shmctl(shmid_shnum, IPC_RMID, NULL);
+	shmctl(shmid_turn, IPC_RMID, NULL);
+	shmctl(shmid_flag, IPC_RMID, NULL);
+
 	exit(0);
 }
+
--- slave.c
+++ slave.c
@@ -6,10 +6,13 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 
+int child_num = 0;		// rank of the child
 int process_num = 0;	// process number of slave
 int max_writes = 0;		// number of times to write to file
+int num_slaves = 0;		// number of sister process, including the current one
 char buffer[26];		// holds the current time when writing to file
 int i = 0;				// iter
+int j = 0;				// iter
 int sleep_time = 0;		// amount of time to sleep before leaving critical 
 						// section
 
@@ -33,15 +36,37 @@ void FormatTimeHMS(char buffer[], int buf_size)
 	strftime(buffer, buf_size, "%H:%M:%S", tm_info);
 }
 
-void PerformCriticalSection()
+// perform the critical section for the num_writeth time and return the remaining number of times to write
+void PerformCriticalSection(int *shm_shnum, char *filename)
 {
-	
+	char msg[100];
+	fprintf(stderr, "Child - %d: I am entering the critical section\n", process_num);
+		
+	sleep_time = rand() % 3;
+	fprintf(stderr, "Child - %d: Going to sleep for %d seconds before incrementing...\n", process_num, sleep_time);
+	sleep(sleep_time);
+	// <<<<< entering critical section >>>>>
+	(*shm_shnum)++; // increment the value in memory
+	FormatTimeHMS(buffer, 26);
+	sprintf(msg, "File modified by process number %d at time %s with shared num %d", process_num, buffer, *shm_shnum);
+	FILE *fp;
+	fp = fopen(filename, "a");
+	fprintf(fp, "%s\n", msg);
+	fclose(fp);
+	// <<<<< leaving critical section >>>>>
+
+	sleep_time = rand() % 3; // sleep for 0-2 seconds
+	fprintf(stderr, "Child - %d: Going to sleep for %d seconds after incrementing...\n", process_num, sleep_time);
+	sleep(sleep_time);
+
+	fprintf(stderr, "Child - %d: I am leaving the critical section\n", process_num);
 }
+
 // waits for it's turn to enter the critical section, writes to a shared 
 // memory variable, and kills itself
 int main(int argc, char *argv[])
 {
-	if (LegalInt(argv[1]) == 0 || LegalInt(argv[2]) == 0)
+	if (LegalInt(argv[1]) == 0 || LegalInt(argv[2]) == 0 || LegalInt(argv[3]) == 0)
 	{
 		printf("%s\n", argv[1]);
 	    fprintf(stderr, "ERROR: invalid args in slave\n");
@@ -50,58 +75,68 @@ int main(int argc, char *argv[])
 	
 	srand(getpid()); // so that each process doesn't use the same seed and 
 					// generate the same sequence of random numbers
-					// 
 
-	process_num = atoi(argv[1]);
+	process_num = getpid();
+	child_num = atoi(argv[1]);
 	max_writes = atoi(argv[2]);
+	num_slaves = atoi(argv[3]);
 	
-	int shmid;
-	key_t key;
-	int *shm;
+	// all of our shared memory variables
+	int turn;
+	int flag[num_slaves]; // 0=idle, 1=want_in, 2=in_cs
+	int sharedNum;
 
-	// get the segment we named previously
-	//if ((key = ftok("/tmp", 199)) == -1)
-	//{
-	//	perror("ftok");
-	//	exit(1);
-	//}
-	key = 5678;
-
-	// locate the segment
-	if ((shmid = shmget(key, 27, 0666)) < 0)
-	{
-		perror("shmget");
-		exit(1);
-	}
-
-	// attach the segment to our data space
-	if ((shm = shmat(shmid, NULL, 0)) == (int *) -1)
-	{
-		perror("shmat");
-		exit(1);
-	}
-
-	for (i = 0; i < max_writes; i++)
-	{
-		fprintf(stderr, "Child - %d: I am entering the critical section\n", process_num);
-		
-		sleep_time = rand() % 3;
-		fprintf(stderr, "Child - %d: Going to sleep for %d seconds before incrementing...\n", process_num, sleep_time);
-		sleep(sleep_time);
-		
-		// <<<<< entering critical section >>>>>
-		(*shm)++; // increment the value in memory		
-		FormatTimeHMS(buffer, 26);
-		printf("File modified by process number %d at time %s with shared num = %d\n", process_num, buffer, *shm);
-		// <<<<< leaving critical section >>>>>
-
-		sleep_time = rand() % 3; // sleep for 0-2 seconds
-		fprintf(stderr, "Child - %d: Going to sleep for %d seconds after incrementing...\n", process_num, sleep_time);
-		sleep(sleep_time);
-
-		fprintf(stderr, "Child - %d: I am leaving the critical section\n", process_num);
-	}
+	key_t key_turn = ftok("/tmp", 35);
+	int shmid_turn = shmget(key_turn, sizeof(int), IPC_CREAT | 0666);
+	int *shm_turn = shmat(shmid_turn, NULL, 0);
+	turn = *shm_turn;
 	
-	shmdt(shm);
+	key_t key_flag = ftok("/tmp", 93);
+	int shmid_flag = shmget(key_flag, sizeof(int)*num_slaves, IPC_CREAT | 0666);
+	int *shm_flag = shmat(shmid_flag, NULL, 0);
+	for (i = 0; i < num_slaves; i++) flag[i] = *(shm_flag + i);
+
+	key_t key_shnum = ftok("/tmp", 27);
+	int shmid_shnum = shmget(key_shnum, sizeof(int), IPC_CREAT | 0666);
+	int *shm_shnum = shmat(shmid_shnum, NULL, 0);
+	sharedNum = *shm_shnum;
+	
+	do {
+		do {
+			*(shm_flag + child_num) = 1; // raise my flag
+			j = *shm_turn;
+
+			// wait until it's my turn
+			while ( j != child_num )
+				j = ( *(shm_flag + j) != 0 ) ? *shm_turn : (j + 1) % num_slaves;
+
+			// declare intention to enter critical section
+			*(shm_flag + child_num) = 2;
+
+			// check that no one else is in critical section
+			for ( j = 0; j < num_slaves; j++ )
+			{
+				if ( (j != child_num) && (*(shm_flag + j) == 2) )
+					break;
+			}
+		} while ( (j < num_slaves) || ((*shm_turn != child_num) && (*(shm_flag + *shm_turn) != 0)) );
+
+		// assign turn to self and enter critical section
+		*shm_turn = child_num;
+		PerformCriticalSection(shm_shnum, argv[4]);
+
+		// exit section
+		max_writes--;
+		j = (*shm_turn + 1) % num_slaves;
+
+		while ( *(shm_flag + j) == 0)
+			j = (j + 1) % num_slaves;
+
+		// assign turn to next waiting process and chang own flag to idle
+		*shm_turn = j;
+		*(shm_flag + child_num) = 0;
+	}	while (max_writes > 0);
+
+	fprintf(stderr, "Child - %d: I am going to die now. Goodbye.\n", process_num);
 	exit(0);
 }
